4d066e3aa327e0f8fd6cf1c4b006c6becedbc331
unpredictable-arcane <arcane.anuj@gmail.com>
2026-09-25 18:06:12 +0530

    Initial commit

---
+++ Java-basics/Start.java
@@ -0,0 +1,20 @@
+class Basics {
+    public static void main(String[] args) {
+//        int a = 67;
+//        if (a == 67) {
+//            System.out.println("Helloooo");
+//        }
+
+//        int count = 1;
+//        while(count != 10) {
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+
+        for(int count = 1; count != 10; count++) {
+            System.out.println(count);
+        }
+    }
+}
