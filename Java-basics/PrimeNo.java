58ab8a127eed7a4c5e75aa143570cf10a3425682
unpredictable-arcane <arcane.anuj@gmail.com>
2026-09-25 18:09:06 +0530

    Add Prime Number file

---
+++ Java-basics/PrimeNo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+
+        int n = in.nextInt();
+        if(n <= 1) {
+            System.out.println("Neither prime nor composite");
+            return;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                System.out.println("Not Prime");
+                return;
+            }
+            c++;
+        }
+            System.out.println("Prime");
+        
+        
+    }
+}
