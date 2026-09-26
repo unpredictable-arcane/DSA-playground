3e00cf237605306ab0679dddef404612f413a081
unpredictable-arcane <arcane.anuj@gmail.com>
2026-09-26 17:04:49 +0530

    Add Primitives  file

---
+++ Java-basics/Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 67;
+        char letter = 'a';
+        float marks = 99.99f;
+        double largeDecimalNumbers = 298734624.3027;
+        long largeInteger = 23472893562528344L;
+        boolean check = false;
+
+        float c = 67.9f;
+        c++;
+//        c = (int)(c) + 1;
+        System.out.println(c);
+    }
+}
